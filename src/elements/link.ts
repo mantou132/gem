3044930e78e5cad4d1de514b9e140531dc47d591
xianqiao.wxq <xianqiao.wxq@alibaba-inc.com>
2019-07-17 21:45:09 +0800

    feat: <link> 支持 href 属性

--- src/elements/link.ts
+++ src/elements/link.ts
@@ -2,9 +2,10 @@ import { GemElement, html, history } from '../';
 import { RouteItem, RouteOptions, createLocation, createPath } from './route';
 
 export class Link extends GemElement {
+  /**@attr */ href: string;
   /**@attr */ path: string;
   /**@attr */ query: string;
-  static observedAttributes = ['path', 'query'];
+  static observedAttributes = ['href', 'path', 'query'];
   static observedStores = [history.historyState];
 
   // 动态路由，根据 route.pattern 和 options.params 计算出 path
@@ -17,29 +18,37 @@ export class Link extends GemElement {
     this.onclick = this.clickHandle;
   }
 
-  get active() {
-    const { path, query } = history.location;
+  getHref() {
     if (this.route) {
       const queryProp = (this.options && this.options.query) || this.query;
-      return path + query === createPath(this.route, this.options) + queryProp;
+      return createPath(this.route, this.options) + queryProp;
     } else {
-      return path + query === this.path + this.query;
+      return this.href || this.path + this.query;
     }
   }
 
   clickHandle = (e: MouseEvent) => {
-    if (!this.active) {
-      e.stopPropagation();
-      if (this.route) {
-        history.pushWithoutCloseHandle(createLocation(this.route, this.options));
-      } else {
-        history.pushWithoutCloseHandle({ path: this.path, query: this.query });
-      }
+    const href = this.getHref();
+    if (!href.startsWith('/')) {
+      window.open(href);
+      return;
+    }
+
+    const { path, query } = history.location;
+    if (path + query === href) return;
+
+    e.stopPropagation();
+    if (this.route) {
+      history.pushWithoutCloseHandle(createLocation(this.route, this.options));
+    } else {
+      history.pushWithoutCloseHandle({ path: this.path, query: this.query });
     }
   };
 
   render() {
-    if (this.active) {
+    const href = this.getHref();
+    const { path, query } = history.location;
+    if (path + query === href) {
       this.setAttribute('active', '');
     } else {
       this.removeAttribute('active');
