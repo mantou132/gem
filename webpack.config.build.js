5db45ef9f012f25f31a0327d9e15fbbc8098dc94
mantou132 <709922234@qq.com>
2019-12-13 11:24:48 +0800

    Add es5 compile target

---
+++ webpack.config.build.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: `./src`,
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  devtool: 'source-map',
+};
