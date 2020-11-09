4ad2911a88d48e76cebb661c4eb6dd1bc355f568
mantou132 <709922234@qq.com>
2020-11-10 07:59:24 +0800

    Add devtools hook

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
+import { updateStore } from './lib/store';
+
+export interface DevToolsHook {
+  updateStore: typeof updateStore;
+}
+
+declare global {
+  interface Window {
+    __GEM_DEVTOOLS__HOOK__?: DevToolsHook;
+  }
+}
+
+if (window.__GEM_DEVTOOLS__HOOK__) {
+  // 不支持多个 gem
+  Object.assign<DevToolsHook, DevToolsHook>(window.__GEM_DEVTOOLS__HOOK__, {
+    updateStore,
+  });
+}
+
 export * from './lib/store';
 export * from './lib/history';
 export * from './lib/element';
