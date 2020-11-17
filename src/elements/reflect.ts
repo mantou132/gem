d2dad8d9b02214425ec99f0715ac488e4162e05c
mantou132 <709922234@qq.com>
2020-11-17 12:04:43 +0800

    Improve `<gem-reflect>`

--- src/elements/reflect.ts
+++ src/elements/reflect.ts
@@ -3,8 +3,6 @@ import { GemElement, customElement, property } from '..';
 /**
  * 将 `<gem-reflect>` 的子元素渲染到其他元素中
  *
- * !!! 不能动态添加和移除子元素；
- *
  * @customElement gem-reflect
  */
 @customElement('gem-reflect')
@@ -17,14 +15,11 @@ export class GemReflectElement extends GemElement {
   }
 
   mounted() {
+    const childNodes = [...this.childNodes];
     this.effect(
-      () => {
-        return () => this.isConnected && console.warn('Changes `target` will not be re-rendered');
-      },
+      () => this.target.append(...childNodes),
       () => [this.target],
     );
-    const childNodes = [...this.childNodes];
-    this.target.append(...childNodes);
     return () => {
       childNodes.forEach((node) => node.remove());
     };
