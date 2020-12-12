b1931d1327d6eb80a664605bcb9435b22f2b7544
mantou132 <709922234@qq.com>
2020-12-13 02:41:41 +0800

    Export `getThemeProp`

--- src/helper/theme.ts
+++ src/helper/theme.ts
@@ -1,23 +1,30 @@
 import { connect, createStore, updateStore, Store } from '../lib/store';
-import { camelToKebabCase, GemError, randomStr } from '../lib/utils';
+import { camelToKebabCase, randomStr } from '../lib/utils';
 
-function replaceStyle(style: HTMLStyleElement, themeStore: Store<Record<string, unknown>>, salt: string) {
-  const rules = Object.keys(themeStore).reduce((prev, key: keyof Store<Record<string, unknown>>) => {
-    return prev + `--${camelToKebabCase(key)}-${salt}:${themeStore[key]};`;
-  }, '');
-  style.textContent = `:root, :host {${rules}}`;
-}
-
-type Theme<T> = {
+type SomeType<T> = {
   [P in keyof T]: string;
 };
 
 const themeStoreMap = new WeakMap();
 
-export function getThemeStore<T>(theme: Theme<T>): Store<T> {
-  const store = themeStoreMap.get(theme);
-  if (!store) throw new GemError('theme not found');
-  return store;
+export function getThemeStore<T>(theme: SomeType<T>) {
+  return themeStoreMap.get(theme) as Store<T>;
+}
+
+const themePropMap = new WeakMap();
+
+export function getThemeProp<T>(theme: SomeType<T>) {
+  return themePropMap.get(theme) as SomeType<T>;
+}
+
+function replaceStyle(style: HTMLStyleElement, theme: Record<string, unknown>) {
+  const themeStore = themeStoreMap.get(theme);
+  const themeProp = themePropMap.get(theme);
+  const rules = Object.keys(themeStore).reduce((prev, key) => {
+    if (!themeStore[key]) return prev;
+    return prev + `${themeProp[key]}:${themeStore[key]};`;
+  }, '');
+  style.textContent = `:root, :host {${rules}}`;
 }
 
 /**
@@ -33,16 +40,19 @@ export function createTheme<T extends Record<string, unknown>>(themeObj: T) {
   const salt = randomStr();
   const style = document.createElement('style');
   const themeStore = createStore<T>(themeObj);
-  const replace = () => replaceStyle(style, themeStore, salt);
+  const theme: Record<string, string> = {};
+  const themeProp: Record<string, string> = {};
+  themePropMap.set(theme, themeProp);
+  themeStoreMap.set(theme, themeStore);
+  Object.keys(themeStore).forEach((key) => {
+    themeProp[key] = `--${camelToKebabCase(key)}-${salt}`;
+    theme[key] = `var(${themeProp[key]})`;
+  });
+  const replace = () => replaceStyle(style, theme);
   connect(themeStore, replace);
   replace();
   (document.head || document.documentElement).append(style);
-  const theme: Record<string, string> = {};
-  themeStoreMap.set(theme, themeStore);
-  Object.keys(themeStore).forEach((key) => {
-    theme[key] = `var(--${camelToKebabCase(key)}-${salt})`;
-  });
-  return theme as Theme<T>;
+  return theme as SomeType<T>;
 }
 
 /**
@@ -50,7 +60,7 @@ export function createTheme<T extends Record<string, unknown>>(themeObj: T) {
  * @param theme 主题
  * @param newThemeObj 新主题
  */
-export function updateTheme<T = Record<string, unknown>>(theme: Theme<T>, newThemeObj: Partial<T>) {
+export function updateTheme<T = Record<string, unknown>>(theme: SomeType<T>, newThemeObj: Partial<T>) {
   const store = getThemeStore(theme);
   updateStore(store, newThemeObj);
 }
