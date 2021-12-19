bbfa5d330f44d5676ba3592617b029773a394110
wangxianqiao <wangxianqiao@cloudfine.com>
2021-12-19 11:35:18 +0800

    Improve mediaquery

--- src/helper/mediaquery.ts
+++ src/helper/mediaquery.ts
@@ -1,70 +1,77 @@
 // https://gist.github.com/gokulkrishh/242e68d1ee94ad05f488
 // https://mediag.com/news/popular-screen-resolutions-designing-for-all/
 
+const matche = (query: string) => window.matchMedia(query).matches;
+
 export const mediaQuery = {
   PRINT: 'print',
   get isPrint() {
-    return window.matchMedia(this.PRINT).matches;
+    return matche(this.PRINT);
   },
   HOVER: '(hover: hover)',
   get isHover() {
-    return window.matchMedia(this.HOVER).matches;
+    return matche(this.HOVER);
   },
 
   PHONE: '(min-width: 319px) and (max-width: 480px)',
   get isPhone() {
-    return window.matchMedia(this.PHONE).matches;
+    return matche(this.PHONE);
   },
 
   LAPTOP: '(min-width: 1023px) and (max-width: 1280px) and (orientation: landscape)',
   get isLaptop() {
-    return window.matchMedia(this.LAPTOP).matches;
+    return matche(this.LAPTOP);
   },
 
   DESKTOP: '(min-width: 1279px) and (orientation: landscape)',
   get isDesktop() {
-    return window.matchMedia(this.DESKTOP).matches;
+    return matche(this.DESKTOP);
+  },
+
+  WIDTHSCREEN: '(min-width: 1919px) and (orientation: landscape)',
+  get isWidthScreen() {
+    return matche(this.WIDTHSCREEN);
   },
 
   PHONE_LANDSCAPE: '(min-width: 479px) and (max-width: 767px) and (orientation: landscape)',
   // PHONE_LANDSCAPE: '(min-width: 481px) and (max-width: 767px)',
   get isPhoneLandscape() {
-    return window.matchMedia(this.PHONE_LANDSCAPE).matches;
+    return matche(this.PHONE_LANDSCAPE);
   },
 
   TABLET: '(min-width: 768px) and (max-width: 1024px)',
   get isTablet() {
-    return window.matchMedia(this.TABLET).matches;
+    return matche(this.TABLET);
   },
 
   SMALL_PHONE: '(min-width: 319px) and (max-width: 480px) and (max-height: 640px)',
   get isSmallPhone() {
-    return window.matchMedia(this.SMALL_PHONE).matches;
+    return matche(this.SMALL_PHONE);
   },
 
   WATCH: '(max-width: 319px)',
   get isWatch() {
-    return window.matchMedia(this.WATCH).matches;
+    return matche(this.WATCH);
   },
 
   SHORT: '(min-width: 479px) and (max-height: 320px)',
   get isShort() {
-    return window.matchMedia(this.SHORT).matches;
+    return matche(this.SHORT);
   },
 
   PWA: '(display-mode: standalone)',
   // PWA: '(display-mode: browser)', // debugging
   get isPWA() {
-    return window.matchMedia(this.PWA).matches;
+    return matche(this.PWA);
   },
 
   MOTION_REDUCE: '(prefers-reduced-motion: reduce)',
   get isMotionReduce() {
-    return window.matchMedia(this.MOTION_REDUCE).matches;
+    return matche(this.MOTION_REDUCE);
   },
 
   DATA_REDUCE: '(prefers-reduced-data: reduce)',
   get isDataReduce() {
-    return window.matchMedia(this.DATA_REDUCE).matches;
+    return matche(this.DATA_REDUCE);
   },
 };
