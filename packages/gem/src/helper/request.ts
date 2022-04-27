192f93ef7b6a1c94776a9c54b9b3e6855d032b38
wangxianqiao <wangxianqiao@cloudfine.com>
2022-04-27 18:52:32 +0800

    Improve request helper

--- packages/gem/src/helper/request.ts
+++ packages/gem/src/helper/request.ts
@@ -1,18 +1,23 @@
-// 带凭证 Rest API 请求
-const defaultReq: RequestInit = {
-  credentials: 'include',
-  mode: 'cors',
+export const defaultReqInit: RequestInit = {
+  headers: {
+    'Content-Type': 'application/json',
+  },
 };
 
 export async function request<T>(uri: string, options: RequestInit = {}): Promise<T> {
   const res = await fetch(uri, {
-    ...defaultReq,
+    ...defaultReqInit,
     ...options,
   });
   if (res.status === 0) throw new Error('Request fail');
   if (res.status >= 500) throw new Error(res.statusText);
-  if (res.status >= 400) throw new Error((await res.text()) || res.statusText);
-  return await res.json();
+  try {
+    const data = await res.json();
+    if (res.status >= 400) throw new (Error as any)(data?.message || res.statusText, { cause: data });
+    return data;
+  } catch {
+    throw new Error('Server error');
+  }
 }
 
 type ReqBody = Record<string, unknown> | BodyInit;
@@ -35,15 +40,17 @@ function serializationBody(body?: ReqBody): BodyInit | undefined {
   return body;
 }
 
-export function get<T>(url: string, params?: Record<string, string | number>) {
+function serializationUrl(url: string, params?: Record<string, string | number>) {
   const { origin, pathname, search } = new URL(url, location.origin);
   const query = new URLSearchParams(params as Record<string, string>).toString();
-  return request<T>(`${origin}${pathname}${search ? `${search}&` : query ? '?' : ''}${query}`, {
-    method: 'GET',
-  });
+  return `${origin}${pathname}${search ? `${search}&` : query ? '?' : ''}${query}`;
 }
-export function del<T>(url: string) {
-  return request<T>(url, { method: 'DELETE' });
+
+export function get<T>(url: string, params?: Record<string, string | number>) {
+  return request<T>(serializationUrl(url, params), { method: 'GET' });
+}
+export function del<T>(url: string, params?: Record<string, string | number>) {
+  return request<T>(serializationUrl(url, params), { method: 'DELETE' });
 }
 export function put<T>(url: string, body?: ReqBody) {
   return request<T>(url, { method: 'PUT', body: serializationBody(body) });
