e3c9e769e95d42c45f955c14490a0900acb92a79
wangxianqiao <wangxianqiao@cloudfine.com>
2022-07-01 16:41:25 +0800

    Close #62

--- packages/duoyun-ui/src/lib/encode.test.ts
+++ packages/duoyun-ui/src/lib/encode.test.ts
@@ -10,10 +10,14 @@ it('`base64ToArrayBuffer`', () => {
 
 it('`utf8ToB64`', () => {
   expect(utf8ToB64('策划师')).to.equal('562W5YiS5biI');
-  expect(utf8ToB64('1234123')).to.equal('MTIzNDEyMw==');
+  expect(utf8ToB64('玩儿1')).to.equal('546p5YS_MQ');
+  expect(utf8ToB64('1234123')).to.equal('MTIzNDEyMw');
 });
 
 it('`base64ToUTF8`', () => {
   expect(b64ToUtf8('562W5YiS5biI')).to.equal('策划师');
+  expect(b64ToUtf8('546p5YS_MQ')).to.equal('玩儿1');
+  expect(b64ToUtf8('546p5YS/MQ==')).to.equal('玩儿1');
+  expect(b64ToUtf8('MTIzNDEyMw')).to.equal('1234123');
   expect(b64ToUtf8('MTIzNDEyMw==')).to.equal('1234123');
 });
--- packages/duoyun-ui/src/lib/encode.ts
+++ packages/duoyun-ui/src/lib/encode.ts
@@ -1,30 +1,35 @@
+function safaUrlToBase64Str(str: string) {
+  return str.replaceAll('-', '+').replaceAll('_', '/');
+}
+
 // https://developer.mozilla.org/en-US/docs/Glossary/Base64#solution_1_%E2%80%93_escaping_the_string_before_encoding_it
 export function b64ToUtf8(str: string) {
   return decodeURIComponent(
     window
-      .atob(
-        // Compatible with jwt
-        str.replace(/-/g, '+').replace(/_/g, '/'),
-      )
+      .atob(safaUrlToBase64Str(str))
       .split('')
       .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
       .join(''),
   );
 }
 
+export function base64ToArrayBuffer(str: string) {
+  return new Uint8Array([...window.atob(safaUrlToBase64Str(str))].map((char) => char.charCodeAt(0))).buffer;
+}
+
+function base64ToSafeUrl(str: string) {
+  return str.replaceAll('+', '-').replaceAll('/', '_').replaceAll('=', '');
+}
+
 export function utf8ToB64(str: string) {
-  return window.btoa(
-    encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (_, p1) => String.fromCharCode(Number(`0x${p1}`))),
+  return base64ToSafeUrl(
+    window.btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (_, p1) => String.fromCharCode(Number(`0x${p1}`)))),
   );
 }
 
 // https://github.com/tc39/proposal-arraybuffer-base64
 export function arrayBufferToBase64(arrayBuffer: ArrayBuffer) {
-  return window.btoa(String.fromCharCode(...new Uint8Array(arrayBuffer)));
-}
-
-export function base64ToArrayBuffer(s: string) {
-  return new Uint8Array([...window.atob(s)].map((char) => char.charCodeAt(0))).buffer;
+  return base64ToSafeUrl(window.btoa(String.fromCharCode(...new Uint8Array(arrayBuffer))));
 }
 
 export async function hash(strOrAb: string | ArrayBuffer, options?: 'string'): Promise<string>;
