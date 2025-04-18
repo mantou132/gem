f9fc448a0fa323786a99a8a278d55f43da3401b0
mantou132 <709922234@qq.com>
2025-04-19 01:07:48 +0800

    [duoyun-ui] Add animation direction option to Vestaboard
    Allows setting the flip animation direction via the `direction`
    attribute ('up' or 'down').
    
    Adds a `debug` attribute to slow down the animation for inspection.

--- packages/duoyun-ui/src/elements/vestaboard.ts
+++ packages/duoyun-ui/src/elements/vestaboard.ts
@@ -1,4 +1,13 @@
-import { adoptedStyle, attribute, customElement, effect, numattribute, part, shadow } from '@mantou/gem/lib/decorators';
+import {
+  adoptedStyle,
+  attribute,
+  boolattribute,
+  customElement,
+  effect,
+  numattribute,
+  part,
+  shadow,
+} from '@mantou/gem/lib/decorators';
 import { createRef, css, GemElement, html } from '@mantou/gem/lib/element';
 
 import { theme } from '../lib/theme';
@@ -16,6 +25,7 @@ const style = css`
     perspective: 2em;
     cursor: default;
     user-select: none;
+    counter-reset: index;
   }
   div {
     position: absolute;
@@ -24,12 +34,23 @@ const style = css`
     background: ${theme.backgroundColor};
     backface-visibility: hidden;
   }
-  .unfoldBottom {
-    z-index: 1;
-    rotate: x -180deg;
+  :host([debug]) div::before {
+    counter-increment: index;
+    content: counter(index);
   }
-  .foldTop {
-    z-index: 2;
+  :host(:not([direction='up'])) {
+    .unfoldBottom {
+      z-index: 1;
+      rotate: x -180deg;
+    }
+    .foldTop {
+      z-index: 2;
+    }
+  }
+  :host([direction='up']) {
+    .unfoldTop {
+      z-index: 2;
+    }
   }
   :host([variant='circle']) {
     div {
@@ -107,7 +128,9 @@ export class DuoyunVestaboardElement extends GemElement {
 
   @attribute char: string;
   @attribute variant: 'circle' | 'polygon';
+  @attribute direction: 'up' | 'down';
   @numattribute duration: number;
+  @boolattribute debug: boolean;
 
   #unfoldTop = createRef<HTMLDivElement>();
   #unfoldBottom = createRef<HTMLDivElement>();
@@ -115,8 +138,30 @@ export class DuoyunVestaboardElement extends GemElement {
   #foldBottom = createRef<HTMLDivElement>();
 
   get #duration() {
-    return this.duration || 500;
+    return this.debug ? 5000 : this.duration || 500;
   }
+
+  get #direction() {
+    return this.direction || 'down';
+  }
+
+  @effect((i) => [i.char])
+  #animation = () => {
+    if (!this.#prevChar) return;
+    const options: EffectTiming = { duration: this.#duration, fill: 'both' };
+
+    if (this.#direction === 'up') {
+      this.#unfoldTop.value!.animate({ rotate: ['x -180deg', 'x 0deg'] }, options);
+      this.#unfoldBottom.value!.animate({ filter: ['brightness(0.5)', 'brightness(1)', 'brightness(1)'] }, options);
+      this.#foldTop.value!.animate({ filter: ['brightness(1)', 'brightness(1)', 'brightness(0.5)'] }, options);
+      this.#foldBottom.value!.animate({ rotate: ['x 0deg', 'x 180deg'] }, options);
+    } else {
+      this.#unfoldTop.value!.animate({ filter: ['brightness(0.5)', 'brightness(1)', 'brightness(1)'] }, options);
+      this.#unfoldBottom.value!.animate({ rotate: ['x 180deg', 'x 0deg'] }, options);
+      this.#foldTop.value!.animate({ rotate: ['x 0deg', 'x -180deg'] }, options);
+      this.#foldBottom.value!.animate({ filter: ['brightness(1)', 'brightness(1)', 'brightness(0.5)'] }, options);
+    }
+  };
 
   #prevChar = '';
   @effect((i) => [i.char])
@@ -124,22 +169,14 @@ export class DuoyunVestaboardElement extends GemElement {
     this.#prevChar = this.char;
   };
 
-  @effect((i) => [i.char])
-  #animation = () => {
-    const options: EffectTiming = { duration: this.#duration, fill: 'both' };
-    this.#unfoldTop.value!.animate({ filter: ['brightness(0.5)', 'brightness(1)', 'brightness(1)'] }, options);
-    this.#unfoldBottom.value!.animate({ rotate: ['x 180deg', 'x 0deg'] }, options);
-    this.#foldTop.value!.animate({ rotate: ['x 0deg', 'x -180deg'] }, options);
-    this.#foldBottom.value!.animate({ filter: ['brightness(1)', 'brightness(1)', 'brightness(0.5)'] }, options);
-  };
-
   render = () => {
+    const prevChar = this.#prevChar || this.char;
     return html`
       <span part=${DuoyunVestaboardElement.before} class="before"></span>
       <div ${this.#unfoldTop} part=${DuoyunVestaboardElement.top} class="unfoldTop">${this.char}</div>
       <div ${this.#unfoldBottom} part=${DuoyunVestaboardElement.bottom} class="unfoldBottom">${this.char}</div>
-      <div ${this.#foldTop} part=${DuoyunVestaboardElement.top} class="foldTop">${this.#prevChar}</div>
-      <div ${this.#foldBottom} part=${DuoyunVestaboardElement.bottom} class="foldBottom">${this.#prevChar}</div>
+      <div ${this.#foldTop} part=${DuoyunVestaboardElement.top} class="foldTop">${prevChar}</div>
+      <div ${this.#foldBottom} part=${DuoyunVestaboardElement.bottom} class="foldBottom">${prevChar}</div>
       <span part=${DuoyunVestaboardElement.after} class="after"></span>
     `;
   };
